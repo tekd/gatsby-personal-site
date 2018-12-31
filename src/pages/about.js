e0478dc2387d84cefbb9e9996faa2629f3458153
Denny Tek <tek.denny@gmail.com>
2018-12-31 13:34:19 -0500

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur,
+      saepe!
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus ad
+      quisquam harum quo quae ipsa nihil illum reprehenderit perferendis iste,
+      minima facere distinctio iure, mollitia, consectetur autem adipisci!
+      Dolorem minima maiores omnis nam dolor accusamus perferendis libero
+      repellendus quidem, quod placeat sapiente, sunt voluptatem alias,
+      temporibus rem modi adipisci aliquam non nemo eius. Voluptatem, excepturi
+      aliquid? Eum, quam. Totam deleniti laborum praesentium! Labore voluptas
+      eligendi aperiam est aliquid pariatur quisquam debitis nesciunt nisi.
+      Deserunt perspiciatis temporibus exercitationem eaque nisi ducimus,
+      aperiam harum rerum quidem excepturi suscipit tempore animi, dolor. At
+      quod quae, odio ipsum a consectetur perferendis reiciendis iusto ipsa.
+    </p>
+  </Layout>
+);
+
+export default AboutPage;
